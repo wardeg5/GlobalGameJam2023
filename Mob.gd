c4fd6320e391c51d2fa7a554c4b93cef9932c5c6
DESKTOP-CA82495\warde <wardeg5@gmail.com>
2023-01-29 22:55:29 -0800

    player movement

---
+++ Mob.gd
@@ -0,0 +1,24 @@
+extends KinematicBody
+
+# Minimum speed of the mob in meters per second.
+export var min_speed = 10
+# Maximum speed of the mob in meters per second.
+export var max_speed = 18
+
+var velocity = Vector3.ZERO
+
+
+func _physics_process(_delta):
+	move_and_slide(velocity)
+
+func initialize(start_position, player_position):
+	look_at_from_position(start_position, player_position, Vector3.UP)
+	rotate_y(rand_range(-PI / 4, PI / 4))
+
+	var random_speed = rand_range(min_speed, max_speed)
+	velocity = Vector3.FORWARD * random_speed
+	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+
+func _on_VisibilityNotifier_screen_exited():
+	queue_free()
